6b6bf5079742a33585521ef9ecd097f65afc3c8d
kaushik397 <kaushi397@gmail.com>
2023-11-29 10:49:32 +0530

    update

--- main.py
+++ main.py
@@ -29,9 +29,10 @@ class Topics(BaseModel):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*"],  # Update as needed
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["*"],
+    allow_methods=["POST"],
+    allow_headers=["*"],  # Update as needed
 )
 
 @app.exception_handler(RequestValidationError)
