998dd1c69de1df695c5c6b28d787a74974c67e43
kaushik397 <kaushi397@gmail.com>
2023-11-30 14:28:41 +0530

    prompt_update

--- questions.py
+++ questions.py
@@ -6,9 +6,16 @@ from langchain.llms import OpenAI
 import json
 import os
 
-def questionGen(topic,apikey):
+def questionGenv1(topic,apikey):
   template="""
-    As a teacher with expertise knowledge on {topic} create 5 questions with distributed difficulty level being a scale of 0 to 3000 in which 0 is easiest and 3000 is hardest and 1500 is medium to test the knowledge of students and teach better and of type MCQ with four different options and one correctAanswer. and return everything in a JSON object with variables question, options, correctanswer, and difficultylevel of question.
+   Create a JSON object containing a list of 5 multiple-choice questions on {topic}. Each question should have the following structure:
+- "question": (The text of the question)
+- "options": (A list of four options)
+- "correctAnswer": (The correct option from the list)
+- "difficultyLevel": (A difficulty level on a scale of 0 to 3000, where 0 is the easiest, 1500 is medium, and 3000 is the hardest)
+
+Ensure that the questions cover various aspects of {topic} and provide meaningful options for each question. Return the JSON structure with the key "questions."
+
   """
   prompt = PromptTemplate(template=template, input_variables=["topic"])
   llm=OpenAI(openai_api_key=apikey,temperature=0.6,max_tokens=1500)
