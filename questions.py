5e84ed01b4a2f588959edb98cf49ce0e1c70c1a1
kaushik397 <kaushi397@gmail.com>
2023-12-09 16:50:44 +0530

    update-v4

--- questions.py
+++ questions.py
@@ -6,7 +6,7 @@ from langchain.llms import OpenAI
 import json
 import os
 
-def questionGen(topic,apikey):
+def questionGen(topic,apikey,max_tokens_limit=1000):
   template="""
    Create a JSON object containing a list of 5 multiple-choice questions on {topic}. Each question should have the following structure:
 - "question": (The text of the question)
@@ -15,13 +15,20 @@ def questionGen(topic,apikey):
 - "difficultyLevel": (A difficulty level on a scale of 0 to 3000, where 0 is the easiest, 1500 is medium, and 3000 is the hardest)
 
 Ensure that the questions cover various aspects of {topic} and provide meaningful options for each question. Return the JSON structure with the key "questions."
-
   """
   prompt = PromptTemplate(template=template, input_variables=["topic"])
-  llm=OpenAI(openai_api_key=apikey,temperature=0.6,max_tokens=1500)
+
+  llm=OpenAI(openai_api_key=apikey,temperature=0.5,max_tokens=max_tokens_limit)
   llm_chain=LLMChain(prompt=prompt, llm=llm)
   response=llm_chain.run({"topic":topic})
-  # print(response)
+  
+  response_length = len(response.split())
+  if response_length>max_tokens_limit:
+    max_tokens_adjusted=max_tokens_limit+(response_length - max_tokens_limit)
+    llm=OpenAI(openai_api_key=apikey,temperature=0.5,max_tokens=max_tokens_adjusted)
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    response=llm_chain.run({"topic":topic})
+
   try:
     responseret=json.loads(response)
     return responseret
